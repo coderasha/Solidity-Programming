9d2bb6d1da48837c229ea5706404a3da46af693e
coderasha <ashadipta@gmail.com>
2022-08-24 23:09:23 +0530

    ternary operator

---
+++ TernaryOperator.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+
+contract ternaryOp{
+
+    
+function cheIf(uint _no) public pure returns (uint){
+uint _val ;
+_val = (_no > 10) ? 100:5 ;
+return _val;
+
+}
+    
+}
