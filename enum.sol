4a16cd7977b8324a79c28292ab872e916f75406a
coderasha <ashadipta@gmail.com>
2022-08-25 20:57:45 +0530

    enum

---
+++ enum.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >=0.8.0 < 0.9.0;
+
+contract Ecommerce{
+
+enum State{
+     Place,
+     Accept,
+     Reject,
+     Ship,
+     Received
+
+}
+State state;
+}
+
+
+
+
+
+
+
+
+}
